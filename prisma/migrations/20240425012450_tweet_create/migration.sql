59f0cdbf1bc78c5b6bc5565f9c418cedc11d3315
Yeongmin_Chae <codudals98@naver.com>
2024-04-25 17:54:28 +0900

    추가 : 프리즈마에 Tweet모델과 Fav모델 추가 후 User모델과 관계 구현

---
+++ prisma/migrations/20240425012450_tweet_create/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Tweet" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Tweet_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
