73065edefdff2d3ed33a3a0f8a066b885f912291
Yeongmin_Chae <codudals98@naver.com>
2024-04-26 15:00:32 +0900

    생성 : DB에서 생성된 tweet GET해오는 API 구현

---
+++ pages/api/tweet/index.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { withApiSession } from "@libs/withSession";
+import db from "@libs/db";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method != "GET") {
+    res.setHeader("Allow", ["GET"]);
+
+    return res.status(405).end().json({ ok: false });
+  } else {
+    try {
+      const tweets = await db.tweet.findMany({
+        include: {
+          _count: {
+            select: {
+              favs: true,
+            },
+          },
+        },
+      });
+
+      if (!tweets) {
+        return res.status(404).json({ ok: false });
+      }
+
+      return res.status(200).json({ tweets, ok: true });
+    } catch (error) {
+      return res.status(500).json({ message: "data가 없습니다", ok: false });
+    }
+  }
+}
+
+export default withApiSession(handler);
