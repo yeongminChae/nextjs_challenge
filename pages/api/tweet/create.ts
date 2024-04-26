8ef340d0f4b49fecb0c45a1db7f764af14859099
Yeongmin_Chae <codudals98@naver.com>
2024-04-26 14:59:31 +0900

    생성 : tweet 생성[POST] 위한 api 구현

---
+++ pages/api/tweet/create.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { withApiSession } from "@libs/withSession";
+import db from "@libs/db";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method != "POST") {
+    res.setHeader("Allow", ["POST"]);
+    return res.status(405).end();
+  } else {
+    const {
+      body: { title, content },
+      session: { user },
+    } = req;
+
+    try {
+      const newTweet = await db.tweet.create({
+        data: {
+          title,
+          content,
+          author: {
+            connect: {
+              id: user?.id,
+            },
+          },
+        },
+      });
+
+      return res.status(201).json({ newTweet, ok: true });
+    } catch {
+      return res.status(500).json({ message: "트윗생성에 실패했습니다." });
+    }
+  }
+}
+
+export default withApiSession(handler);
