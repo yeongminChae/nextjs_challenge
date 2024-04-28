f1f5ae111cfd0c1c61bcf5a173125873f4c6850d
Yeongmin_Chae <codudals98@naver.com>
2024-04-28 22:27:20 +0900

    추가 : 홈페이지에 그려지는 트윗에 작가내용 추가하기 + date-fns 라이브러리를 사용해서, 시간 더 쉽게 관리하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from "react";
 import { NextPage } from "next";
-import { useRouter } from "next/router";
 import Link from "next/link";
 
 import useSWR from "swr";
 import { Tweet } from "@prisma/client";
+import { formatDistanceToNow } from "date-fns";
+import { ko } from "date-fns/locale";
 
 import { isUserLoggedIn } from "@libs/client/isUserLoggedIn";
 
 export interface ITweetstResponseWithCount extends Tweet {
   _count: {
     favs: number;
+  };
+  author: {
+    name: string;
   };
 }
 
@@ -24,14 +28,14 @@ const Home: NextPage = () => {
   const { data, error } = useSWR<ITweetResponse>("/api/tweet");
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen gap-10">
+    <div className="flex flex-col items-center justify-center w-full h-screen gap-10 max-h-max">
       <h1> Hello {user?.name} </h1>
-      <div className="flex flex-col gap-3">
+      <div className="flex gap-3">
         <Link href="/profile">
-          <a> Link: To My profile page </a>
+          <a> To My profile page </a>
         </Link>
         <Link href="/tweet/create">
-          <a> Link: Tweet Create Page </a>
+          <a> To Tweet Create </a>
         </Link>
       </div>
       {data?.tweets?.map((tweet) => {
@@ -41,11 +45,19 @@ const Home: NextPage = () => {
             id={tweet?.id + ""}
             className="flex flex-col gap-5"
           >
+            <div className="flex gap-5">
+              <Link href={`/tweet/${tweet?.id}`}>
+                <a>{tweet?.title}</a>
+              </Link>
+              <span>By : {tweet?.author?.name}</span>
+              <span>{timeSince(tweet?.createdAt).toString()}</span>
+              <span>❤️ : {tweet?._count?.favs}</span>
+            </div>
             <Link href={`/tweet/${tweet?.id}`}>
-              <a>{tweet?.title}</a>
+              <a>
+                <p>{tweet?.content}</p>
+              </a>
             </Link>
-            <span>좋아요 수 : {tweet?._count?.favs}</span>
-            <p>{tweet?.content}</p>
           </div>
         );
       })}
@@ -54,3 +66,8 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+const timeSince = (date: Date) => {
+  const result = formatDistanceToNow(date, { addSuffix: true, locale: ko });
+  return result;
+};
