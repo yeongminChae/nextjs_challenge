b30395ce477118900f3eff1288d9fc1c25864dfc
Yeongmin_Chae <codudals98@naver.com>
2024-04-28 22:31:12 +0900

    구현 : 로그아웃 기능 구현

--- pages/api/auth/profile.ts
+++ pages/api/auth/profile.ts
@@ -4,11 +4,7 @@ import { withApiSession } from "@libs/server/withSession";
 import db from "@libs/server/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method != "GET") {
-    res.setHeader("Allow", ["GET"]);
-
-    return res.status(405).end().json({ ok: false });
-  } else {
+  if (req.method == "GET") {
     try {
       const profile = await db.user.findUnique({
         where: {
@@ -24,6 +20,18 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     } catch (error) {
       return res.status(500).json({ message: "data가 없습니다", ok: false });
     }
+  } else if (req.method == "POST") {
+    try {
+      req.session.destroy();
+      return res.status(200).json({ ok: true });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "로그아웃 중 에러가 발생했습니다.", ok: false });
+    }
+  } else {
+    res.setHeader("Allow", ["GET", "POST"]);
+    return res.status(405).end().json({ ok: false });
   }
 }
 
